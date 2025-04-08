7b608fb4ca4a5fe2540ad4ac7621f480e97ed263
lordsepticplier <126767712+lordsepticplier@users.noreply.github.com>
2025-04-08 17:34:51 +1200

    Update test_calc.py

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,5 @@
 import pytest
-from calc.calc import plus,subtract,multiply,calculate
+from calc.calc import plus,subtract,calculate
 
 def test_add () :
   assert plus(5 , 2) == 7
@@ -10,12 +10,12 @@ def test_subtract () :
   assert subtract(5 , 2) == 3
   assert subtract(1 ,4) == -3
   assert subtract(-2 , -2) == 0
-
+"""
 def test_multiply () :
   assert multiply(2 , 2) == 4
   assert multiply(-1 , 3) == -3
   assert multiply(-1 , -1) == 1
-
+"""
 def test_calculate () :
   assert calculate ('add' , 5 , 2) == 7
   assert calculate ('subtract' , 6 , 3) == 3
